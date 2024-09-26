c640086fc1b576e6a9b24e8b33f7a09a70b2d1f0
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-09-25 21:23:10 -0300

    Criado Timer Para a Data do Evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 AOS.init();
+
+const eventDate = new Date("Nov 12, 2024 19:00:00");
+const eventTimeStamp = eventDate.getTime();
+
+const countDate = setInterval(function() {
+    const now = new Date();
+    const nowTimeStamp = now.getTime();
+
+    const timeUntilEvent = eventTimeStamp - nowTimeStamp;
+
+    const daysInMS = 1000 * 60 *60 * 24;
+    const hoursInMS = 1000 * 60 * 60;
+    const minutesinMS = 1000 * 60;
+
+    const daysUntilEvent = Math.floor(timeUntilEvent / (daysInMS));
+    const hoursUntilEvent = Math.floor((timeUntilEvent % (daysInMS)) / (hoursInMS));
+    const minutesUntilEvent = Math.floor(timeUntilEvent % (hoursInMS) / (minutesinMS));
+    const secondsUntilEvent = Math.floor((timeUntilEvent % minutesinMS) / 1000)
+
+    document.getElementById('contador').innerHTML = `começa em ${daysUntilEvent}d ${hoursUntilEvent}h ${minutesUntilEvent}m ${secondsUntilEvent}s`;
+
+    if (timeUntilEvent < 0) {
+        clearInterval(countDate);
+        document.getElementById('contador').innerHTML = '<br>Evento expirado'
+    }
+}, 1000)
